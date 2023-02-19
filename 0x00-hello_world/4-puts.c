38fd53feb70e0bbf7caa041a730ae6c336a2d915
Abebe Arusa <abebe.argo@gmail.com>
2023-02-19 10:49:51 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include < stdio.h >
+/**
+  * main A program that prints a line with a function called pusts
+  *Return: 0 (yes the program is right)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
