4ade49de816f4b9b2c7c387cf9ebe70f13c4dbdf
Abebe Arusa <abebe.argo@gmail.com>
2023-02-19 11:38:04 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main A program that print a line using printf function
-* Return : 0 (success)
+* main - A program that print a line using printf function
+* Return : 0 (shows the success of the program)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
 
