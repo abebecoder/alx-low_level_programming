55124fd36fccf0098bbef25450fc8660ade571ba
Abebe Arusa <abebe.argo@gmail.com>
2023-02-19 11:17:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main A program that prints a line with a function called pusts
+* main - A program that prints a line with a function called pusts
 * Return: 0 (yes the program is right)
 */
 int main(void)
