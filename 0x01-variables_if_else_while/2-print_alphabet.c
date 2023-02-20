1e3d158e4e4e0bbe625d926c0e72ee76cf02b44c
Abebe Arusa <abebe.argo@gmail.com>
2023-02-20 01:28:25 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Retrurn: 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
