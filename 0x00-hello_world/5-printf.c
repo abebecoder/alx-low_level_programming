1c711944c7b6fe9b30d2e713b30eae87488f9ed6
Abebe Arusa <abebe.argo@gmail.com>
2023-02-26 12:25:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - A program that print a line using printf function
+ *
  * Return: 0 (Shows the success of the program)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
