a22e8931ebf1578863d62002a6030f0cb7060976
Abebe Arusa <abebe.argo@gmail.com>
2023-03-22 13:12:00 -0700

    C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,65 +1,24 @@
-#include "3-calc.h"
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
 
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 
 
-/**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-/**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The difference of a and b.
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The product of a and b.
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The quotient of a and b.
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The remainder of the division of a by b.
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
